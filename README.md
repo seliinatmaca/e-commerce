47853af54d6ac4fe27b2909c62537174eaa936df
seliinatmaca <selinatmacayy@gmail.com>
2024-01-14 02:05:36 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS-E Commerce
+Javascript  Project that has basics of e-commerce # e-commerce
---
+++ main.js
@@ -0,0 +1,28 @@
+const categoryList = document.querySelector(".categories");
+
+function fetchCategories() {
+  // veri çekme isteği atma
+  fetch('https://api.escuelajs.co/api/v1/categories')
+    // gelen veriyi işleme
+    .then((res) => res.json())
+    // işlenen veriyi foreach ile herbir obje için ekrana basma
+    .then((data) =>
+      data.slice(0, 4).forEach((category) => {
+        const { image, name } = category;
+        // gelen herbir obje için div oluşturma
+        const categoryDiv = document.createElement('div');
+        // dive class ekleme
+        categoryDiv.classList.add('category');
+        // divin içeriğini değiştirme
+        categoryDiv.innerHTML = `
+            <img src="${image}" />
+            <span>${name}</span>
+        `;
+        // oluşan divi htmldeki listeye atma
+        categoryList.appendChild(categoryDiv);
+      })
+    );
+}
+
+
+
